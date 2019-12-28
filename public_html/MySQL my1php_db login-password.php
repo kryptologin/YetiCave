1ac25254d4004c1340b7ca9ef64d23c892d4595b
kryptologin <59101123+kryptologin@users.noreply.github.com>
2019-12-29 00:00:02 +0300

    Deleted temporary files

--- public_html/MySQL my1php_db login-password.php
+++
@@ -1,3 +0,0 @@
-	
-Имя Базы				Пользователь			пароль				Хост	 
-id10743467_my1php_db	id10743467_bearhythm	qwedsqzxc1982AP		localhost 
--- public_html/templates/404.tpl.php
+++
@@ -1,9 +0,0 @@
-<main>
-  <section class="lot-item container">
-    <h2>Извините, такого лота не существует</h2>
-    <div class="lot-item__content">
-      <div class="lot-item__left">
-          <img src="/img/404.png" width="730" height="548" alt="404">
-      </div>
-    </div>
-</main>
